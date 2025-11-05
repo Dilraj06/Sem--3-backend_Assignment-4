bd1c8b72f05dd0785c0b366f130caded411594d6
Dilraj06 <balnoor004@gmail.com>
2025-11-04 20:17:05 -0600

    defined authorizationOptions

---
+++ src/api/v1/models/authorizationOptions.ts
@@ -0,0 +1,12 @@
+/**
+* Defines the options for the authorization middleware.
+* 
+* @param hasRole - An array of allowed roles (limited to 'admin', 'manager', 'officer', and 'user')
+* @param allowSameUser - Optional boolean flag indicating whether a user can access their own resources
+* @example 
+* { hasRole: ["admin", "manager"], allowSameUser: true } as AuthorizationOptions
+*/
+export interface AuthorizationOptions {
+    hasRole: Array<"admin" | "manager" | "officer" | "user">;
+    allowSameUser?: boolean;
+}
