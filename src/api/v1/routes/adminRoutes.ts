0fe69928ac1975db52579aab88ca27c1dd87b6f0
Dilraj06 <balnoor004@gmail.com>
2025-11-04 23:37:40 -0600

    made some changes in adminroutes file

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { Router } from "express";
 import { setCustomClaims } from "../controllers/adminController";
 import authenticate from "../middleware/authenticate";
 import isAuthorized from "../middleware/authorize";
  
-const router = express.Router();
+const router: Router = express.Router();
  
 // POST /api/v1/admin/setCustomClaims - admin only
 router.post(
